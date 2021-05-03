37f8c68b5cc6bfde8b2e3edfc5dfdca19e4e2bab
Moisés Ñañez <moisesnandres@hey.com>
2021-05-03 01:01:31 -0500

    Show date

--- components/Entry.js
+++ components/Entry.js
@@ -7,11 +7,18 @@ export default function Entry({ entry }) {
     </article>
   }
 
+  let date = new Date(entry.data.created);
+  const dd = String(date.getDate()).padStart(2, '0');
+  const mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+  const yyyy = date.getFullYear();
+
+  date = mm + '/' + dd + '/' + yyyy;
+
   return (
     <article className={styles.container}>
       <h2>{entry.data.title}</h2>
       <p>Author: {entry.data.author}</p>
-
+      <p>{date}</p>
       {
         entry.data.thumbnail ?
           <a href={entry.data.url} target='_blank'>
